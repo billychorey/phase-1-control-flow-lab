0f82fdef83b67266ca1dc00ced05fd4a52a8db9c
Billy <billychorey@gmail.com>
2023-07-18 19:12:43 -0400

    phase one control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLength) {
+  let message;
+  if (rideLength <= 400) {
+    message = "This one is on me!";
+  } else if (400 < rideLength && rideLength < 2000) {
+    message = "That will be twenty bucks.";
+  } else if (2000 < rideLength && rideLength < 2500) {
+    message = "I will gladly take your thirty bucks.";
+  } else if (rideLength > 2500) {
+    message = "No can do.";
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+scuberGreetingForFeet();
+
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+ternaryCheckCity();
+
+function switchOnCharmFromTip(tip) {
+  let response;
+  switch (tip) {
+    case "generous":
+      response = "Thank you so much.";
+      return response;
+      break;
+    case "not as generous":
+      response = "Thank you.";
+      return response;
+      break;
+    default:
+      response = "Bye.";
+      return response;
+      break;
+  }
 }
